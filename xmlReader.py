b7c1098b1fccab7371f4b21ec2bd4d4f378d33c5
SHON <elton.shon@keysight.com>
2016-07-17 22:52:23 -0700

    move xmlModifier to xmlReader

--- xmlReader.py
+++ xmlReader.py
@@ -88,6 +88,84 @@ def fileFormatIncorrectWarning(fileName):
 	messagebox.showinfo("Warning", "File: " + fileName + " - format incorrect!") #### maybe make another interface
 
 
+def modifier(xmlFolderPath, xmlFileName, refList, nameList, typeList, referenceE, wireE, tree):
+	### make a ElementTree object and find its root (highest node)   
+	root = tree.getroot() 
+	### make two lists of all reference elements(objects) and wire elements(objects)
+	numOfRef = len(referenceE)
+	numOfWire = len(wireE)
+	for n in range(0, len(refList)):
+		### Create reference(copy) with according names, types, and dependency
+		### And insert them after the last referenceSystem if ref entry is not empty
+		if refList[n]:
+			for r in referenceE:
+				if 'R' + refList[n] == r.find('Name').text:
+					print("im here")
+					copy = writeARefCopy(r, refList[n], nameList[n], typeList[n])
+					root.insert(int(nameList[n])-1, copy) ################## error caused by insert with random index??
+					break
+
+		### change wire's des		
+		modifyWireDesRef(refList[n], nameList[n], wireE)
+
+	### write to a new xml file
+	if xmlFileName[-1].isdigit() and xmlFileName[-5:-1] == "_new":
+		newXmlFilePath = xmlFolderPath + "/" + xmlFileName[0:-1] + str(int(xmlFileName[-1])+1) + ".xml"
+	else:
+		newXmlFilePath = xmlFolderPath + "/" + xmlFileName + "_new1.xml"
+	tree.write(newXmlFilePath)
+	return newXmlFilePath
+
+def writeARefCopy(refToCopy, oldName, newName, typ): 
+	### creat new referenceSystem node
+	newRefEle = Element('ReferenceSystem')
+	newNameEle = SubElement(newRefEle, 'Name')
+	newNameEle.text = 'R' + newName
+	newTypeEle = SubElement(newRefEle, 'Type')
+	newTypeEle.text = typ
+	newDepEle = SubElement(newRefEle, 'Dependon')
+	newDepEle.text = 'R' + oldName
+	### creat two nodes that refer to original points objects(elements)
+	for p in refToCopy.findall('Point'):
+		newRefEle.append(p)
+	### formatting xml text so it prints nicly 
+	indent(newRefEle, 1)
+	### return the reference(address) of the ref element
+	return newRefEle
+
+def modifyWireDesRef(oldDes, newDes, wireElement):
+	# print("old: " + oldDes)
+	# print("new: " + newDes +'\n')
+	for wire in wireElement:
+		secBondDes = wire.findall('Bond')[1].find('Refsys')
+		# print("wire's des: " + secBondDes.text)
+		if secBondDes.text == 'R' + oldDes:
+			secBondDes.text = 'R' + newDes
+			# print("changed to: " + secBondDes.text)
+		# else:
+			# print("no changes")
+		# print("\n")
+
+### in-place prettyprint formatter found online --> http://effbot.org/zone/element-lib.htm#prettyprint 
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+
+
+
+
 
 
 
