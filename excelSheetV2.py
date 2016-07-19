124922d27f3dbc35f4a357750b846a788405a835
SHON <elton.shon@keysight.com>
2016-07-19 10:39:49 -0700

    fixed error for append tag indexing and added conditional formatting

--- excelSheetV2.py
+++ excelSheetV2.py
@@ -35,9 +35,11 @@ class excelSheet():
 		missingTagAndRefF = workbook.add_format({'valign': 'vcenter', 'align': 'center', 'bg_color': '#FFC7CE', 'font_color': '#9C0006', 'border': 1, 'border_color': '#b2b2b2'   })
 		missingUnblockedF = workbook.add_format({'valign': 'vcenter', 'align': 'center', 'bg_color': '#FFC7CE', 'locked': 0, 'border': 1, 'border_color': '#b2b2b2'})
 		missingDepBlockedF = workbook.add_format({'valign': 'vcenter', 'align': 'center', 'bg_color': '#FFC7CE', 'locked': 1, 'hidden': 1,'border': 1, 'border_color': '#b2b2b2'})
+		missingDepBlockedBlankF = workbook.add_format({'valign': 'vcenter', 'align': 'center', 'bg_color': '#FFC7CE', 'font_color': '#FFC7CE', 'locked': 1, 'hidden': 1,'border': 1, 'border_color': '#b2b2b2'})
 		appendTagAndRefF = workbook.add_format({'valign': 'vcenter', 'align': 'center', 'font_color': 'white', 'bg_color': '#92cddc', 'locked': 1, 'border': 1, 'border_color': '#b2b2b2'})
-		appendDepBlockedF = workbook.add_format({'valign': 'vcenter', 'align': 'center', 'bg_color': '#92cddc', 'locked': 1, 'hidden': 1, 'border': 1, 'border_color': '#b2b2b2'})
 		appendUnblockedF =  workbook.add_format({'valign': 'vcenter', 'align': 'center', 'bg_color': '#92cddc', 'locked': 0,'border': 1, 'border_color': '#b2b2b2'})
+		appendDepBlockedF = workbook.add_format({'valign': 'vcenter', 'align': 'center', 'bg_color': '#92cddc', 'locked': 1, 'hidden': 1, 'border': 1, 'border_color': '#b2b2b2'})
+		appendDepBlockedBlankF = workbook.add_format({'valign': 'vcenter', 'align': 'center', 'bg_color': '#92cddc', 'font_color': '#92cddc', 'locked': 1, 'hidden': 1, 'border': 1, 'border_color': '#b2b2b2'})
 
 		### activate protection
 		worksheet.protect('elton')
@@ -70,6 +72,7 @@ class excelSheet():
 					worksheet.write(self.copyC + rowS, None,  missingUnblockedF)
 					worksheet.write(self.typeC + rowS, None,  missingUnblockedF)
 					worksheet.write_formula(self.depC + rowS, '=' + self.copyC + rowS, missingDepBlockedF)
+					worksheet.conditional_format(self.depC + rowS, {'type': 'cell', 'criteria': 'equal to', 'value': 0, 'format': missingDepBlockedBlankF})
 					worksheet.data_validation(self.copyC + rowS, {'validate': 'list', 'source': refNumList,'error_title': 'Warning', 'error_message': 'Reference does not exist!', 'error_type': 'stop'}) 
 				else:  ### existing ref row
 					worksheet.write(self.refC + rowS, refNumber, centerF)
@@ -79,16 +82,14 @@ class excelSheet():
 					# print(refListIndex)
 					refListIndex += 1
 			else: ### append section
+				worksheet.write(self.statusC + rowS, self.appendTag, appendTagAndRefF)
 				worksheet.write(self.refC + rowS, refNumber, appendTagAndRefF)
 				worksheet.write(self.copyC + rowS, None, appendUnblockedF)
 				worksheet.write(self.typeC + rowS, None,  appendUnblockedF)
 				worksheet.write_formula(self.depC + rowS, '=' + self.copyC + rowS, appendDepBlockedF)
+				worksheet.conditional_format(self.depC + rowS, {'type': 'cell', 'criteria': 'equal to', 'value': 0, 'format': appendDepBlockedBlankF})
 				worksheet.data_validation(self.copyC + rowS, {'validate': 'list', 'source': refNumList,'error_title': 'Warning', 'error_message': 'Reference does not exist!', 'error_type': 'stop'})
 			refNumber = refNumber + 1
-		if fstAppendRow == lastAppendRow:
-			worksheet.write(self.statusC + fstAppendRow, self.appendTag, appendTagAndRefF)
-		else:
-			worksheet.merge_range(self.statusC + fstAppendRow + ':' + self.statusC + lastAppendRow, self.appendTag, appendTagAndRefF)
 
 		### add comment
 		copyTitleComment = 'Input a name of any existing refernces from the XML file (number only).\nAll gaps need to be filled out'
@@ -108,10 +109,6 @@ class excelSheet():
 		startfile(xlsxFilePath)
 
 	def readExcelSheet(self, xlsxFilePath):
-		excelName = []
-		excelRef = []
-		excelType = []
-		excelDependon = []
 		workbook = op.load_workbook(filename = xlsxFilePath, read_only=True)
 		try:
 			worksheet = workbook.get_sheet_by_name(self.workSheetName)
@@ -124,33 +121,36 @@ class excelSheet():
 		
 		xmlFilePath = worksheet[self.xmlFilePathCell].value[5:]
 
-		row  = self.firstInputRow
-		tagCo = worksheet.columns[0]
-		lastRow = len(tagCo)
-		excelName = []
+		
+		
 		excelRef = []
+		excelCopy = []
 		excelType = []
-		missingRef = []
+		excelDep = []
+		missingCopy = [] ### missingCopy = []
 		missingType = []
 		wrongSeqRow = []
 		checkRepeatRef = set()
 		temp = {}
 		repeat = {}
-		gap = tagCo[int(row)].value == self.gapTag
+		row  = self.firstInputRow
+		statusCo = worksheet.columns[0]
+		lastRow = len(statusCo)
+		IsAppend = statusCo[int(row)].value == self.appendTag
 		prevBothNotEmpty = True
 		while int(row) <= lastRow: 
 			name = worksheet[self.nameC + row].value
 			ref = str(worksheet[self.refC + row].value)
 			typ = worksheet[self.typeC + row].value
 			### gap
-			if gap:
+			if not IsAppend:
 				if ref and typ and ref != 'None' and typ != 'None':
 					excelName.append(name)
-					excelRef.append(ref)
+					missingCopy.append(ref)
 					excelType.append(typ)
 				else:
 					if not ref or ref == 'None':
-						missingRef.append(row)
+						missingCopy.append(row)
 					if not typ or typ == 'None':
 						missingType.append(row)
 				if tagCo[int(row)].value == self.appendTag:
@@ -158,10 +158,10 @@ class excelSheet():
 			else: ### append
 				if ref and typ and  ref != 'None' and typ != 'None' and prevBothNotEmpty:
 					excelName.append(name)
-					excelRef.append(ref)
+					missingCopy.append(ref)
 					excelType.append(typ)
 				elif (not ref or ref == 'None') and typ and typ != 'None' and prevBothNotEmpty:
-					missingRef.append(row)              
+					missingCopy.append(row)              
 				elif (not typ or typ != 'None') and ref and ref != 'None' and prevBothNotEmpty:
 					missingType.append(row)
 				elif prevBothNotEmpty:
@@ -170,7 +170,7 @@ class excelSheet():
 					wrongSeqRow.append(row)
 					prevBothNotEmpty = True
 					if not (ref and ref != 'None'):
-						missingRef.append(row)
+						missingCopy.append(row)
 					elif not (typ and typ != 'None'):
 						missingType.append(row)
 
@@ -189,13 +189,13 @@ class excelSheet():
 		# print(excelName)
 		# print(excelRef)
 		# print(excelType)		
-		# print(missingRef)
+		# print(missingCopy)
 		# print(missingType)
 		# print(repeat)
 		# print(wrongSeqRow)
 		errorMessage = None
-		if missingRef or missingType or repeat or wrongSeqRow:
-			errorMessage = writeErrorMessage(missingRef, missingType, repeat, wrongSeqRow)
+		if missingCopy or missingType or repeat or wrongSeqRow:
+			errorMessage = writeErrorMessage(missingCopy, missingType, repeat, wrongSeqRow)
 			print(errorMessage)
 		return xmlFilePath, excelName, excelRef, excelType, errorMessage
 
